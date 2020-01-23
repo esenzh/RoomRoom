938742b8e898fc56ef522af66c5034ec71e99921
Rudolf <rudolf.bexshtedt@mail.ru>
2020-01-23 14:35:58 +0300

    Release form

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.post('/api/newForm', function(req, res, next) {
+  console.log(req.body)
   res.send('ok');
 });
 
