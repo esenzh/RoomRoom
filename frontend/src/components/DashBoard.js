c9d22eefb6b3d672b7617761737d775a4e0aab3c
Rudolf <rudolf.bexshtedt@mail.ru>
2020-01-28 18:20:05 +0300

    add check date in form

--- frontend/src/components/DashBoard.js
+++ frontend/src/components/DashBoard.js
@@ -24,7 +24,7 @@ class DashBoard extends Component {
   }
 
   isLike = async () => {
-    console.log('inLike')
+    console.log(this.state.id)
     const reqComparison = await fetch(
       '/api/sendLikeMail',
       {
@@ -38,7 +38,7 @@ class DashBoard extends Component {
       });
     let users = await reqComparison.json();
     message.success(users.text);
-    console.log(users)
+    // console.log(users)
   }
 
   showModal = (user) => {
