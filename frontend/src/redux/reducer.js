19393eea5a66d1c26e504a16516cdbae875f0e8c
Rudolf <rudolf.bexshtedt@mail.ru>
2020-01-29 20:21:28 +0300

    add redux dashboard

--- frontend/src/redux/reducer.js
+++ frontend/src/redux/reducer.js
@@ -3,14 +3,16 @@ import {
   ADD_ISLOGIN,
   ADD_LIKED_BY_USERS,
   ADD_MUTUAL_USERS,
-  REMOVE_LIKED_BY_USERS
+  REMOVE_LIKED_BY_USERS,
+  ADD_USERS_DASHBOARD
 } from "./actions";
 
 const initialState = {
   photos: [],
   isLogin: false,
   likedByUsers: [],
-  mutualUsers: []
+  mutualUsers: [],
+  usersDashBoard:[]
 };
 
 export default function(oldState = initialState, action) {
@@ -20,28 +22,32 @@ export default function(oldState = initialState, action) {
         photos: action.photo,
         isLogin: oldState.isLogin,
         likedByUsers: [...oldState.likedByUsers],
-        mutualUsers: [...oldState.mutualUsers]
+        mutualUsers: [...oldState.mutualUsers],
+        usersDashBoard: [...oldState.usersDashBoard]
       };
     case ADD_ISLOGIN:
       return {
         photos: oldState.photos,
         isLogin: action.isLogin,
         likedByUsers: [...oldState.likedByUsers],
-        mutualUsers: [...oldState.mutualUsers]
+        mutualUsers: [...oldState.mutualUsers],
+        usersDashBoard: [...oldState.usersDashBoard]
       };
     case ADD_LIKED_BY_USERS:
       return {
         photos: oldState.photos,
         isLogin: oldState.isLogin,
         likedByUsers: action.likedByUsers,
-        mutualUsers: [...oldState.mutualUsers]
+        mutualUsers: [...oldState.mutualUsers],
+        usersDashBoard: [...oldState.usersDashBoard]
       };
     case ADD_MUTUAL_USERS:
       return {
         photos: oldState.photo,
         isLogin: oldState.isLogin,
         likedByUsers: [...oldState.likedByUsers],
-        mutualUsers: action.mutualUsers
+        mutualUsers: action.mutualUsers,
+        usersDashBoard: [...oldState.usersDashBoard]
       };
     case REMOVE_LIKED_BY_USERS:
       const newlikedByUsers = oldState.likedByUsers.filter(
@@ -51,8 +57,17 @@ export default function(oldState = initialState, action) {
         photos: action.photo,
         isLogin: oldState.isLogin,
         likedByUsers: newlikedByUsers,
-        mutualUsers: [...oldState.mutualUsers]
+        mutualUsers: [...oldState.mutualUsers],
+        usersDashBoard: [...oldState.usersDashBoard]
       };
+    case ADD_USERS_DASHBOARD:
+      return {
+        photos: oldState.photos,
+        isLogin: oldState.isLogin,
+        likedByUsers: [...oldState.likedByUsers],
+        mutualUsers: [...oldState.mutualUsers],
+        usersDashBoard:action.users
+      }
 
     default:
       return oldState;
