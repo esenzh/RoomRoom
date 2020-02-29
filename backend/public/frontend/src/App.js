e30216a3a3db08daf39103bc72ff5076ec57c950
Esen Zheenchoroev <stefunices@gmail.com>
2020-02-29 19:29:31 +0300

    refactor: add routes to second scenario

--- backend/public/frontend/src/App.js
+++ backend/public/frontend/src/App.js
@@ -13,9 +13,12 @@ import Logout from "./components/Logout";
 import DashBoard from "./components/DashBoard";
 import EditProfile from "./components/EditProfile";
 import Landing from "./components/Landing";
-import Register_where from "./components/user_owner/register_where/Register_where";
-import Register_who from "./components/user_owner/register_who/Register_who";
-import Register_you from "./components/user_owner/register_you/Register_you";
+import Register_where_owner from "./components/user_owner/register_where/Register_where";
+import Register_who_owner from "./components/user_owner/register_who/Register_who";
+import Register_you_owner from "./components/user_owner/register_you/Register_you";
+import Register_where_noowner from "./components/user_no_owner/registerNoOwnerWhere/RegisterWhereNoOwner";
+import Register_who_noowner from "./components/user_no_owner/registerNoOwnerWho/RegisterWhoNoOwner";
+import Register_you_noowner from "./components/user_no_owner/registerNoOwnerYou/RegisterYouNoOwner";
 
 
 class App extends React.Component {
@@ -30,9 +33,12 @@ class App extends React.Component {
             <Route exact path={"/anketa"} component={WrappedDynamicRule} />
             <Route exact path={"/profile"} component={Profile} />
             <Route path={"/profile/edit"} component={EditProfile} />
-            <Route exact path={"/signup/where"} component={Register_where} />
-            <Route exact path={"/signup/who"} component={Register_who} />
-            <Route exact path={"/signup/you"} component={Register_you} />
+            <Route exact path={"/signup/where_owner"} component={Register_where_owner} />
+            <Route exact path={"/signup/who_owner"} component={Register_who_owner} />
+            <Route exact path={"/signup/you_owner"} component={Register_you_owner} />
+            <Route exact path={"/signup/where"} component={Register_where_noowner} />
+            <Route exact path={"/signup/who"} component={Register_who_noowner} />
+            <Route exact path={"/signup/you"} component={Register_you_noowner} />
             <Route exact path={"/login"} component={Login} />
             <Route exact path={"/signup"} component={Signup} />
             <Route exact path={"/logout"} component={Logout} />
