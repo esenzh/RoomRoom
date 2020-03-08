7a8329470779d1a2ee038779c72a668172b5da49
RuWolf <rudolf.bexshtedt@mail.ru>
2020-03-08 17:17:33 +0300

    add dash owner

--- backend/public/frontend/src/components/DashBoardOwner.js
+++ backend/public/frontend/src/components/DashBoardOwner.js
@@ -140,182 +140,182 @@ class DashBoard extends Component {
     return (
       <div>
         <br/>
-        {this.state.loading && (
-          <div style={{textAlign: "center"}}>
-            <Spin size="large" tip="Загрузка..."/>
-          </div>
-        )}
-        {this.props.users && (
-          <p style={{fontSize: "25px"}} align={"center"}>
-            Подходящие для Вас пользователи!
-          </p>
+        {/*{this.state.loading && (*/}
+        {/*  <div style={{textAlign: "center"}}>*/}
+        {/*    <Spin size="large" tip="Загрузка..."/>*/}
+        {/*  </div>*/}
+        {/*)}*/}
+        {/*{this.props.users && (*/}
+        {/*  <p style={{fontSize: "25px"}} align={"center"}>*/}
+        {/*    Подходящие для Вас пользователи!*/}
+        {/*  </p>*/}
 
-        )}
-        <Collapse>
-          <Panel header="Дополнительные фильтры" key="1">
-            <div style={{marginLeft: 'auto', marginRight: 'auto', width: '250px'}}>
-              <div style={{textAlign: "center"}}>
-                <Switch defaultChecked onChange={this.onChangeSexMan}/> Показывать мужчин
-              </div>
-              <div style={{textAlign: "center"}}>
-                <Switch defaultChecked onChange={this.onChangeSexWoman}/> Показывать женщин
-              </div>
-              <div style={{marginLeft: 'auto', marginRight: 'auto', width: '250px'}}>
-                <Slider range value={[this.state.minPrice, this.state.maxPrice]} max={150} onChange={this.onChangePrice}
-                        defaultValue={[this.state.minPrice, this.state.maxPrice]}
-                        marks={{0: '0 т.р.', 150: '150 т.р.'}}/>Бюджет
-              </div>
-              <div style={{marginLeft: 'auto', marginRight: 'auto', width: '250px'}}>
-                <Slider range max={100} min={18} value={[this.state.minAge, this.state.maxAge]}
-                        onChange={this.onChangeAge}
-                        defaultValue={[this.state.minAge, this.state.maxAge]} marks={{18: '18 лет', 100: '100 лет'}}/>Возраст
-              </div>
-              <Form>
-                <Form.Item label="Метро" hasFeedback>
-                  <div>
-                    <Select
-                      defaultValue={provinceData[0]}
-                      onChange={this.handleProvinceChange}
-                    >
-                      {provinceData.map(province => (
-                        <Option key={province}>
-                          <Badge color={colorMetro[province]}/>
-                          {province}
-                        </Option>
-                      ))}
-                    </Select>
-                    <Select
-                      mode="multiple"
-                      placeholder="Please select"
-                      onChange={this.onSecondCityChange}
-                      style={{width: '100%'}}
-                    >
-                      {cities.map(city => (
-                        <Option value={city} key={city}>
-                          {city}
-                        </Option>
-                      ))}
-                    </Select>
-                  </div>
-                </Form.Item>
-              </Form>
-              <Button onClick={this.searchMetro} type="primary" icon="search">Поиск</Button>
-            </div>
-          </Panel>
+        {/*)}*/}
+        {/*<Collapse>*/}
+        {/*  <Panel header="Дополнительные фильтры" key="1">*/}
+        {/*    <div style={{marginLeft: 'auto', marginRight: 'auto', width: '250px'}}>*/}
+        {/*      <div style={{textAlign: "center"}}>*/}
+        {/*        <Switch defaultChecked onChange={this.onChangeSexMan}/> Показывать мужчин*/}
+        {/*      </div>*/}
+        {/*      <div style={{textAlign: "center"}}>*/}
+        {/*        <Switch defaultChecked onChange={this.onChangeSexWoman}/> Показывать женщин*/}
+        {/*      </div>*/}
+        {/*      <div style={{marginLeft: 'auto', marginRight: 'auto', width: '250px'}}>*/}
+        {/*        <Slider range value={[this.state.minPrice, this.state.maxPrice]} max={150} onChange={this.onChangePrice}*/}
+        {/*                defaultValue={[this.state.minPrice, this.state.maxPrice]}*/}
+        {/*                marks={{0: '0 т.р.', 150: '150 т.р.'}}/>Бюджет*/}
+        {/*      </div>*/}
+        {/*      <div style={{marginLeft: 'auto', marginRight: 'auto', width: '250px'}}>*/}
+        {/*        <Slider range max={100} min={18} value={[this.state.minAge, this.state.maxAge]}*/}
+        {/*                onChange={this.onChangeAge}*/}
+        {/*                defaultValue={[this.state.minAge, this.state.maxAge]} marks={{18: '18 лет', 100: '100 лет'}}/>Возраст*/}
+        {/*      </div>*/}
+        {/*      <Form>*/}
+        {/*        <Form.Item label="Метро" hasFeedback>*/}
+        {/*          <div>*/}
+        {/*            <Select*/}
+        {/*              defaultValue={provinceData[0]}*/}
+        {/*              onChange={this.handleProvinceChange}*/}
+        {/*            >*/}
+        {/*              {provinceData.map(province => (*/}
+        {/*                <Option key={province}>*/}
+        {/*                  <Badge color={colorMetro[province]}/>*/}
+        {/*                  {province}*/}
+        {/*                </Option>*/}
+        {/*              ))}*/}
+        {/*            </Select>*/}
+        {/*            <Select*/}
+        {/*              mode="multiple"*/}
+        {/*              placeholder="Please select"*/}
+        {/*              onChange={this.onSecondCityChange}*/}
+        {/*              style={{width: '100%'}}*/}
+        {/*            >*/}
+        {/*              {cities.map(city => (*/}
+        {/*                <Option value={city} key={city}>*/}
+        {/*                  {city}*/}
+        {/*                </Option>*/}
+        {/*              ))}*/}
+        {/*            </Select>*/}
+        {/*          </div>*/}
+        {/*        </Form.Item>*/}
+        {/*      </Form>*/}
+        {/*      <Button onClick={this.searchMetro} type="primary" icon="search">Поиск</Button>*/}
+        {/*    </div>*/}
+        {/*  </Panel>*/}
 
-        </Collapse>
+        {/*</Collapse>*/}
 
 
         <div className="dashBoardContainer">
           <div className="dashBoardContent">
-            {this.props.users &&
+            {/*{this.props.users &&*/}
 
-            this.props.users.map((user, i) => {
-              if (this.filterPrise(user.prise) && this.filterSex(user.sex) && this.filterAge(user.age)) {
+            {/*this.props.users.map((user, i) => {*/}
+            {/*  if (this.filterPrise(user.prise) && this.filterSex(user.sex) && this.filterAge(user.age)) {*/}
 
-                let srcImg;
-                if (user.photo[0]) {
-                  srcImg = user.photo[0].thumbUrl;
-                } else {
-                  srcImg = 'https://alawarkey.at.ua/images/avatar.png';
-                }
-                return (
+            {/*    let srcImg;*/}
+            {/*    if (user.photo[0]) {*/}
+            {/*      srcImg = user.photo[0].thumbUrl;*/}
+            {/*    } else {*/}
+            {/*      srcImg = 'https://alawarkey.at.ua/images/avatar.png';*/}
+            {/*    }*/}
+            {/*    return (*/}
 
-                  <div key={i}>
-                    <Card
-                      onClick={() => this.showModal(user)}
-                      className="userCard"
-                      cover={
-                        <img
-                          style={{borderRadius: "10px 10px 0px 0px"}}
-                          alt="example"
-                          src={srcImg}
-                        />
-                      }
-                    >
-                      <div>
-                        <h3 style={{float: "left"}}>
-                          {user.first_name} {user.age}
-                        </h3>
-                      </div>
-                    </Card>
-                  </div>
-                );
-              }
-            })}
+            {/*      <div key={i}>*/}
+            {/*        <Card*/}
+            {/*          onClick={() => this.showModal(user)}*/}
+            {/*          className="userCard"*/}
+            {/*          cover={*/}
+            {/*            <img*/}
+            {/*              style={{borderRadius: "10px 10px 0px 0px"}}*/}
+            {/*              alt="example"*/}
+            {/*              src={srcImg}*/}
+            {/*            />*/}
+            {/*          }*/}
+            {/*        >*/}
+            {/*          <div>*/}
+            {/*            <h3 style={{float: "left"}}>*/}
+            {/*              {user.first_name} {user.age}*/}
+            {/*            </h3>*/}
+            {/*          </div>*/}
+            {/*        </Card>*/}
+            {/*      </div>*/}
+            {/*    );*/}
+            {/*  }*/}
+            {/*})}*/}
           </div>
 
-          {this.state.modalUser && (
-            <Modal
-              title="Детальная информация"
-              visible={this.state.visible}
-              onCancel={this.handleCancel}
-              footer={[
-                <div style={{height: 60}}>
-                  <Icon
-                    type="close-circle"
-                    style={{fontSize: "62px", float: "left"}}
-                    onClick={this.handleCancel}
-                  />
-                  <Icon
-                    type="heart"
-                    theme="twoTone"
-                    twoToneColor="#eb2f96"
-                    style={{fontSize: "62px", float: "right"}}
-                    onClick={this.isLike}
-                  />
-                </div>
-              ]}
-            >
-              <div style={{textAlign: 'center'}}>
-                <Carousel autoplay>
-                  {this.state.modalUser.foto.map((f, i) =>
-                    <div key={i}>
-                      <Avatar size={180} src={f}/>
-                    </div>
-                  )}
-                </Carousel>
-              </div>
-              <div style={{height: '40px'}}>
+          {/*{this.state.modalUser && (*/}
+          {/*  <Modal*/}
+          {/*    title="Детальная информация"*/}
+          {/*    visible={this.state.visible}*/}
+          {/*    onCancel={this.handleCancel}*/}
+          {/*    footer={[*/}
+          {/*      <div style={{height: 60}}>*/}
+          {/*        <Icon*/}
+          {/*          type="close-circle"*/}
+          {/*          style={{fontSize: "62px", float: "left"}}*/}
+          {/*          onClick={this.handleCancel}*/}
+          {/*        />*/}
+          {/*        <Icon*/}
+          {/*          type="heart"*/}
+          {/*          theme="twoTone"*/}
+          {/*          twoToneColor="#eb2f96"*/}
+          {/*          style={{fontSize: "62px", float: "right"}}*/}
+          {/*          onClick={this.isLike}*/}
+          {/*        />*/}
+          {/*      </div>*/}
+          {/*    ]}*/}
+          {/*  >*/}
+          {/*    <div style={{textAlign: 'center'}}>*/}
+          {/*      <Carousel autoplay>*/}
+          {/*        {this.state.modalUser.foto.map((f, i) =>*/}
+          {/*          <div key={i}>*/}
+          {/*            <Avatar size={180} src={f}/>*/}
+          {/*          </div>*/}
+          {/*        )}*/}
+          {/*      </Carousel>*/}
+          {/*    </div>*/}
+          {/*    <div style={{height: '40px'}}>*/}
 
-              </div>
-              <p>
-                <div style={{color: 'black'}}>Ищу квартиру для совместной аренды рядом с метро:</div>
-                <div style={{fontSize: '20px'}}>{this.state.modalUser.location}</div>
-              </p>
+          {/*    </div>*/}
+          {/*    <p>*/}
+          {/*      <div style={{color: 'black'}}>Ищу квартиру для совместной аренды рядом с метро:</div>*/}
+          {/*      <div style={{fontSize: '20px'}}>{this.state.modalUser.location}</div>*/}
+          {/*    </p>*/}
 
-              <p>
-                <div style={{color: 'black'}}>Мои интересы:</div>
-                <div style={{fontSize: '20px'}}>{this.state.modalUser.interest.join(", ")}</div>
-              </p>
-              <p>
-                <div style={{color: 'black'}}>Совпавшие
-                  интересы: {this.state.modalUser.сomparisonInterests.length} </div>
-                <div style={{fontSize: '20px'}}>{this.state.modalUser.сomparisonInterests.join(", ")}</div>
-              </p>
-              {this.state.modalUser.nativeLocation &&
-              <p>
-                <div style={{color: 'black'}}>Родной город:</div>
-                <div style={{fontSize: '20px'}}>{this.state.modalUser.nativeLocation}</div>
-              </p>
-              }
-              <p>
-                <div style={{color: 'black'}}>О себе:</div>
-                <div style={{fontSize: '20px'}}>{this.state.modalUser.about}</div>
-              </p>
-              <p>
-                <div style={{color: 'black'}}>Мой бюджет аренды:</div>
-                <div style={{fontSize: '20px'}}>{this.state.modalUser.prise} т.р.</div>
-              </p>
-            </Modal>
+          {/*    <p>*/}
+          {/*      <div style={{color: 'black'}}>Мои интересы:</div>*/}
+          {/*      <div style={{fontSize: '20px'}}>{this.state.modalUser.interest.join(", ")}</div>*/}
+          {/*    </p>*/}
+          {/*    <p>*/}
+          {/*      <div style={{color: 'black'}}>Совпавшие*/}
+          {/*        интересы: {this.state.modalUser.сomparisonInterests.length} </div>*/}
+          {/*      <div style={{fontSize: '20px'}}>{this.state.modalUser.сomparisonInterests.join(", ")}</div>*/}
+          {/*    </p>*/}
+          {/*    {this.state.modalUser.nativeLocation &&*/}
+          {/*    <p>*/}
+          {/*      <div style={{color: 'black'}}>Родной город:</div>*/}
+          {/*      <div style={{fontSize: '20px'}}>{this.state.modalUser.nativeLocation}</div>*/}
+          {/*    </p>*/}
+          {/*    }*/}
+          {/*    <p>*/}
+          {/*      <div style={{color: 'black'}}>О себе:</div>*/}
+          {/*      <div style={{fontSize: '20px'}}>{this.state.modalUser.about}</div>*/}
+          {/*    </p>*/}
+          {/*    <p>*/}
+          {/*      <div style={{color: 'black'}}>Мой бюджет аренды:</div>*/}
+          {/*      <div style={{fontSize: '20px'}}>{this.state.modalUser.prise} т.р.</div>*/}
+          {/*    </p>*/}
+          {/*  </Modal>*/}
 
-          )}
+          {/*)}*/}
         </div>
-        <footer style={{backgroundColor: '#4A76A8', color: '#ffffff', margin: '0 auto', width: "80%"}} align={"center"}>
-          <p>Всего пользователей в RoomRoom: {this.state.usersLength}</p>
+        {/*<footer style={{backgroundColor: '#4A76A8', color: '#ffffff', margin: '0 auto', width: "80%"}} align={"center"}>*/}
+        {/*  <p>Всего пользователей в RoomRoom: {this.state.usersLength}</p>*/}
 
-          <div dangerouslySetInnerHTML={{__html: this.ym()}}/>
-        </footer>
+        {/*  <div dangerouslySetInnerHTML={{__html: this.ym()}}/>*/}
+        {/*</footer>*/}
       </div>
     );
   }
