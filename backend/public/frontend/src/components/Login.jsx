5d91b403170b664e7c1701068476896b0422dd0c
Esen Zheenchoroev <stefunices@gmail.com>
2020-03-15 16:41:58 +0300

    refactor: redirect to profile page after login

--- backend/public/frontend/src/components/Login.jsx
+++ backend/public/frontend/src/components/Login.jsx
@@ -56,7 +56,7 @@ class Login extends Component {
 
     render() {
         if (this.state.isRedirect) {
-            return <Redirect to={'/'} />
+            return <Redirect to={'/profile'} />
         }
         const { getFieldDecorator } = this.props.form;
         return (
